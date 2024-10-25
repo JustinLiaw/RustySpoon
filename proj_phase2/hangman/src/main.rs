92af960ed2a956c9eb019f6b655d61e5eb5023fe
DavidkodiCS <m253312@usna.edu>
2024-10-25 09:34:22 -0400

    added some functions

--- proj_phase2/hangman/src/main.rs
+++ proj_phase2/hangman/src/main.rs
@@ -1,5 +1,17 @@
 //words.txt file from https://github.com/Xethron/Hangman/blob/master/words.txt
 
+fn pick_word(){
+
+}
+
+fn build_hangman(){
+
+}
+
+fn letter_in_word(){
+
+}
+
 fn main() {
     println!("Welcome to Hangman!");
 }
