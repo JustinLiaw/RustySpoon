69c0be858300452b749e6e3f19f7374bd090ecd9
JustinLiaw <m253642@usna.edu>
2024-11-07 16:39:56 -0500

    Added Phase 2 Readme, formatting, blanks for the word, and code comments

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # RustySpoon
 
+Phase 1
+
 Direction for Compilation:
 
 
@@ -19,3 +21,20 @@ For Execution:
 All code will run all parts at the beginning. If you want to isolate a specific 
 part comment out the rest of the sections in main. No function definitions need to
 be commented out to run. 
+
+Phase 2:
+
+1. git clone https://github.com/JustinLiaw/RustySpoon.git
+
+2. cd proj_phase2
+
+3. cargo run
+
+Description and Utilization of Rust: 
+    We made a hangman game. This game pulls a random word from a text file
+    and then plays the standard hangman game. The main feature of rust is
+    the fact that it is memory safe. You will notice this in the requirement 
+    to explicitly name variable mutable. The code took considerably longer 
+    becuase we kept having to find which varible were meant to be mutable and
+    which ones were not. Furthermore, the file read in is also a build-in memory
+    safe function. 
--- proj_phase2/hangman/src/main.rs
+++ proj_phase2/hangman/src/main.rs
@@ -5,13 +5,27 @@ extern crate console;
 use rand::{thread_rng, Rng};
 use console::Term;
 
+
+//Stuct that conatins the fields of the Game implemntation
+//secret is the desired words
+//discovered_word contains the correct guesses of the word and has '_' for placeholders
+//game_on determins if there the game is ready to stop or not
+//guessed_letters contains all the guesses the user has made
+//stage contains which file number stage to print next 
+
 struct Game {
     secret: String,
     discovered_word: Vec<char>,
     game_on : bool,
-    guessed_letters: Vec<char>,
+    guessed_letters: Vec <char>,
     stage: i32
 }
+
+
+//impl for the Game
+    //Constructs the a Game instance
+    //Contains methods: stage, get_get_status, end_game, add_to_guess, print_guesses, print_discovered_word, get_stage
+    //check_hit, and won  
 
 impl Game {
     pub fn new(secret: String, discovered_word: Vec<char>, game_on: bool, guessed_letters: Vec<char>, stage: i32) -> Self {
@@ -23,7 +37,7 @@ impl Game {
             stage
         }
     }
-
+    //Reads in a stage file and prints it to the screen
     pub fn stage(&self) -> (){
         let stage = format!("hangman/src/stage{}.txt", self.stage);
         let file = fs::read_to_string(stage).expect("File Read");
@@ -39,7 +53,9 @@ impl Game {
     }
 
     pub fn add_to_guesses(&mut self, guess: char) -> (){
-        self.guessed_letters.push(guess);
+        if !self.guessed_letters.contains(&guess) {
+            self.guessed_letters.push(guess);
+        }
     }
 
     pub fn print_guesses(&self) -> (){
@@ -47,13 +63,13 @@ impl Game {
     }
 
     pub fn print_discovered_word(&mut self) -> (){
-        for i in 0..self.secret.len() {
-            if self.guessed_letters.contains(&(self.secret.as_bytes()[i] as char)) && !self.discovered_word.contains(&(self.secret.as_bytes()[i] as char)) {
-                self.discovered_word.push(self.secret.as_bytes()[i] as char);
-            } 
-
-        }
-        println!("{:#?}", self.discovered_word);
+        // for i in 0..self.secret.len() {
+        //     if self.guessed_letters.contains(&(self.secret.as_bytes()[i] as char)) && !self.discovered_word.contains(&(self.secret.as_bytes()[i] as char)) {
+        //         println!("yes");
+        //         self.discovered_word[i] = self.secret.as_bytes()[i] as char;
+        //     }
+        // }
+        println!("{:?}", self.discovered_word);
     }
 
     pub fn get_stage(&self) -> i32 {
@@ -61,27 +77,44 @@ impl Game {
     }
 
     pub fn check_hit(&mut self, guess: char) -> () {
+        // println!("------------------------------");
+        // if self.guessed_letters.contains(&guess) {
+        //     println!("Already Tried this one you silly little sea cookie, try again!");
+        // }
         if self.secret.contains(&guess.to_string()) {
+            for i in 0..self.secret.len() {
+                if self.guessed_letters.contains(&(self.secret.as_bytes()[i] as char)) { //&& !self.discovered_word.contains(&(self.secret.as_bytes()[i] as char)) {
+                    self.discovered_word[i] = self.secret.as_bytes()[i] as char;
+                }
+            }
             println!("The secret word contains: {}", &guess.to_string());
-        }else{
+        }
+        else{
+            println!("MISS! The secret word does not contain: {}", &guess.to_string());
             self.stage += 1;
         }
+        
     }
 
+
     pub fn won(&self) -> bool {
-        if self.secret.len() == self.discovered_word.len(){
+        if self.discovered_word.contains(&'_') == false {
             return true;
-        }else{
+        }
+        else    {
             return false;
         }
     }
 }
 
+//picks a random word from a sting set
 
 fn pick_word(words: &[String]) -> &str{
     let index = thread_rng().gen_range(0..=words.len());
     return &words[index];
 }
+
+//reads in a word file
 
 fn build_word_array() -> Vec<String>{
     let file = fs::read_to_string("hangman/src/words.txt").expect("File Read");
@@ -94,7 +127,11 @@ fn build_word_array() -> Vec<String>{
     return words;
 }
 
+
+
 fn main() {
+    //init the game standards
+
     let _rng = rand::thread_rng();
     //Dynamic Array of Words
     let words = build_word_array();
@@ -103,12 +140,25 @@ fn main() {
     let secret: String = pick_word(&words).to_lowercase();
     //println!("{secret}");
 
-    let mut hangman = Game::new(secret.clone(), Vec::<char>::new(), true, Vec::<char>::new(), 0);
+    //Game new(secret, discovered_word, game_on, guessed_letters, stage)
+    let mut hangman = Game::new(secret.clone(),vec!['_'; secret.len()],  true,Vec::<char>::new(), 0);
     let term = Term::stdout();
 
+    //While game is running
+    //1. Print guesses that have been made
+    //2. Print all the discovered_word
+    //3. Print the stage
+    //4. ASk for user input
+    //6. Add Guess to guess list
+    //7. Check to see if there was a hit 
+    //8. Check winning
+    
     while hangman.get_game_status(){
+        
         print!("Guesses: ");
         hangman.print_guesses();
+
+        //Prints the blanks and the correct guesses
 
         println!("Secret Word: ");
         hangman.print_discovered_word();
@@ -118,15 +168,18 @@ fn main() {
         let guess: char = term.read_char().expect("Unable to Read");
         println!("{guess}");
         
+        
         hangman.add_to_guesses(guess);
 
         hangman.check_hit(guess);
+        
 
         if hangman.get_stage() == 7 {
-            println!("You died!\nThe secret word was {secret}");
+            println!("You died!\nThe secret word was {secret}!");
             hangman.end_game();
         }else if hangman.won() {
-            println!("Congrats, you won!");
+            println!("-------------------------------");
+            println!("\n\nCongrats, you won! The word was {secret}!");
             hangman.end_game();
         }
     }  
